5d50520235fef073452e036859b6cd26cac0e8e8
wangerfei <83889573@qq.com>
2022-04-14 18:55:30 +0800

    api 时间戳验证

--- spring-security/security-demo/src/main/java/xyz/ersut/security/securitydemo/config/security/openapi/filter/OpenAPIFilter.java
+++ spring-security/security-demo/src/main/java/xyz/ersut/security/securitydemo/config/security/openapi/filter/OpenAPIFilter.java
@@ -1,21 +1,32 @@
 package xyz.ersut.security.securitydemo.config.security.openapi.filter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 import xyz.ersut.security.securitydemo.config.security.openapi.token.OpenAPIAuthenticationToken;
 import xyz.ersut.security.securitydemo.config.security.openapi.token.OpenAPIPrincipal;
+import xyz.ersut.security.securitydemo.utils.WebUtils;
+import xyz.ersut.security.securitydemo.utils.result.ResultJson;
+import xyz.ersut.security.securitydemo.utils.result.code.ResultSystemCode;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
+import java.util.Objects;
 
 @Slf4j
 @Component
@@ -23,9 +34,19 @@ public class OpenAPIFilter extends OncePerRequestFilter {
 
     public static final String OPEN_API_PREFIX = "/api";
 
+    //header头的key
+    private static final String HEADER_APPID = "_appId";
+    private static final String HEADER_SIGN = "_sign";
+    private static final String HEADER_TIMESTAMP = "_timestamp";
+
+    //超时时间
+    private static final int OVERTIME = 30000;
+
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -37,11 +58,17 @@ public class OpenAPIFilter extends OncePerRequestFilter {
             return;
         }
 
-        //校验时间戳
+        /**校验时间戳*/
+        String timestamp = request.getHeader(HEADER_TIMESTAMP);
+        if (!timestampVerify(response,timestamp)) {
+            log.info("时间戳出错,url:[{}]，请求中的时间戳：[{}]",request.getRequestURI(),timestamp);
+            return;
+        }
+
+        //获取签名
 
         //获取参数
 
-        //获取签名
 
         //排序参数并拼装字符串
 
@@ -68,4 +95,31 @@ public class OpenAPIFilter extends OncePerRequestFilter {
         filterChain.doFilter(request,response);
 
     }
+
+    /**
+     * 时间戳检验
+     */
+    private boolean timestampVerify(HttpServletResponse response,String timestamp) throws JsonProcessingException {
+        //判断是不是空的
+        if(ObjectUtils.isEmpty(timestamp)){
+            //非法请求 打回
+            log.info("没有时间戳");
+            WebUtils.renderString(response, HttpStatus.BAD_REQUEST.value(), objectMapper.writeValueAsString(ResultJson.generateResultJson(ResultSystemCode.ILLEGAL)));
+            return false;
+        }
+        Long timestampLong = new Long(timestamp);
+
+        //获取当前时间戳并校验请求中的时间戳(上下30秒)
+        long timestampNow = new Date().getTime();
+        if((timestampNow - OVERTIME) > timestampLong || timestampLong > (timestampNow + OVERTIME)){
+            //访问超时 打回
+            log.info("时间戳过期");
+            WebUtils.renderString(response, HttpStatus.BAD_REQUEST.value(), objectMapper.writeValueAsString(ResultJson.generateResultJson(ResultSystemCode.TIMEOUT)));
+            return false;
+        }
+        return true;
+    }
+
+
+
 }
--- spring-security/spring security 集成api加密.md
+++ spring-security/spring security 集成api加密.md
@@ -1,26 +1,37 @@
 # spring security 集成api加密
 
-**接口前缀：/openapi**
+**接口前缀：/api**
 
 ### 接口加密逻辑
 
 1. 接口提供方提供 appId 和 appkey
-2. 必要参数
+2. 必要参数（通过header传输）
    1. \_appId：调用方id(上面提到的appId)，接口提供方通过这个参数识别调用方
    2. \_sign：调用接口的签名，根据调用的参数计算得出，服务端通过这个参数防止伪装请求/防止篡改
-   3. \_timestamp：13位时间戳，通过时间戳验证请求的有效性
-3. 对除签名外的所有请求参数按key做的升序排列,value无需编码。（假设当前时间的时间戳是12345678）例如：有c=3,b=2,a=1 三个参，另加上时间戳后， 按key排序后为：a=1，b=2，c=3，\_appid=xxx，\_timestamp=12345678
-4. 把参数名和参数值连接成字符串，得到拼装字符：a=1&b=2&c=3&\_appid=xxx&_timestamp=12345678
-5. 用申请到的appkey 连接到接拼装字符串尾部，然后进行32位MD5加密
-   1. 示例：假设appkey=test，md5(a=1&b=2&c=3&\_appid=xxx&_timestamp=12345678&appkey=test)，取得MD5摘要值36af22a258478500b74ed4d2af8685ae
-6. 最终发送请求的参数：a=1，b=2，c=3，\_appid=xxx，\_timestamp=12345678，\_sign=36af22a258478500b74ed4d2af8685ae
+   3. \_timestamp：13位时间戳，通过时间戳验证请求的有效性，此处使用北京时间
+3. GET请求
+   1. 将所有请求参数按key做的升序排列,value无需编码。（假设当前时间的时间戳是12345678）例如：有c=3,b=2,a=1 三个参，另加上时间戳和_appId后， 按key排序后为：a=1，b=2，c=3，\_appid=xxx，\_timestamp=12345678
+   2. 把参数名和参数值连接成字符串，得到拼装字符：a=1&b=2&c=3&\_appid=xxx&_timestamp=12345678
+   3. 用申请到的appkey 连接到接拼装字符串尾部，然后进行32位MD5加密
+      1. 示例：假设appkey=test，md5(a=1&b=2&c=3&\_appid=xxx&_timestamp=12345678&\_appkey=test)，取得MD5值36af22a258478500b74ed4d2af8685ae
+   4. 最终发送的请求
+      1. 参数：a=1，b=2，c=3
+      2. header：\_appid=xxx，\_timestamp=12345678，\_sign=36af22a258478500b74ed4d2af8685ae
+
+4. POST请求(json数据)
+   1. 在将json串后拼接时间戳、\_appId以及appkey。假设当前时间的时间戳是12345678 ，appid是xxx , appkey是test，json为{"b":1,"a":2}。最终拼接完后是{"b":1,"a":2}\_appid=xxx&_timestamp=12345678&\_appkey=test
+   2. 计算签名，MD5进行加密 md5({"b":1,"a":2}\_appid=xxx&_timestamp=12345678&\_appkey=test) ,那么签名就是a5352e2105915492013e2b93484c218a
+   3. 最终发送的请求
+      1. json数据：{"b":1,"a":2}
+      2. header：\_appid=xxx，\_timestamp=12345678，\_sign=a5352e2105915492013e2b93484c218a
+
 
 ### 服务端（接口提供方）验证请求
 
-1. 获取参数中的时间戳，与服务端现在的时间戳比较上下未超出 30秒为合法请求
-2. 获取所有参数去除_sign参数后将其余参数按照 接口加密逻辑 中的3、4步 拼装字符串
-3. 获取参数中的appid，在数据库中查询对应的appkey
-4. 按照 接口加密逻辑 中的第5步计算签名，将计算出的签名与请求中签名对比，一致则合法请求
+1. 获取header中的时间戳，与服务端现在的时间戳比较上下未超出 30秒为合法请求
+1. 获取header中的\_appId，在数据库中查询对应的appkey
+2. 按照 接口加密逻辑 中的3、4步 计算签名
+4. 获取header中的\_sign，将计算出的签名与请求中签名对比，一致则合法请求
 
 ### 在spring security中集成
 
