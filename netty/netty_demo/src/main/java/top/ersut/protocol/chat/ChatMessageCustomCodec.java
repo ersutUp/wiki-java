94034317770ccb875ea046920531653dab3ad0c5
wangerfei <83889573@qq.com>
2024-01-16 09:57:40 +0800

    【netty】自定义协议-解码demo以及测试

--- netty/netty_demo/src/main/java/top/ersut/protocol/chat/ChatMessageCustomCodec.java
+++ netty/netty_demo/src/main/java/top/ersut/protocol/chat/ChatMessageCustomCodec.java
@@ -1,22 +1,24 @@
 package top.ersut.protocol.chat;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageCodec;
+import lombok.extern.slf4j.Slf4j;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-
+@Slf4j
 public class ChatMessageCustomCodec extends ByteToMessageCodec<Message> {
 
     @Override
-    protected void encode(ChannelHandlerContext ctx, Message msg, ByteBuf out) throws Exception {
+    public void encode(ChannelHandlerContext ctx, Message msg, ByteBuf out) {
         //魔数
-        out.writeBytes(new byte[]{1,2,3,4});
+        out.writeBytes(new byte[]{1, 2, 3, 4});
         //版本号
         out.writeByte(msg.getVersion());
-        //序列化类型
+        //序列化方式
         out.writeByte(msg.getSerialization().val);
         //消息类型
         out.writeByte(msg.getMessageType().val);
@@ -38,6 +40,37 @@ public class ChatMessageCustomCodec extends ByteToMessageCodec<Message> {
 
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        //魔数
+        byte magic[] = new byte[4];
+        in.readBytes(magic);
+        //版本号
+        byte version = in.readByte();
+        //序列化方式
+        byte serialization = in.readByte();
+        //消息类型
+        byte msgType = in.readByte();
+        //序号
+        int sequenceId = in.readInt();
+        //补充字节
+        in.readByte();
 
+        //长度
+        int length = in.readInt();
+
+        if (length != 0){
+            byte[] content = new byte[length];
+            in.readBytes(content);
+
+            //根据序列化方式 处理消息内容
+            if(SerializationTypeEnum.JSON.val == serialization){
+                String s = new String(content, StandardCharsets.UTF_8);
+                JsonElement jsonTree = new Gson().toJsonTree(s);
+                log.info("收到数据:[{}]",jsonTree);
+                //将读取的内容传给下一个处理器
+                out.add(jsonTree);
+            }
+        } else {
+            log.info("长度值与数据不匹配");
+        }
     }
 }
--- netty/netty_demo/src/main/test/top/ersut/netty/protocol/chat/ChatMessageCustomCodecTest.java
+++ netty/netty_demo/src/main/test/top/ersut/netty/protocol/chat/ChatMessageCustomCodecTest.java
@@ -1,5 +1,7 @@
 package top.ersut.netty.protocol.chat;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.handler.logging.LoggingHandler;
 import org.junit.Test;
@@ -9,15 +11,53 @@ import top.ersut.protocol.chat.LoginRequestMessage;
 public class ChatMessageCustomCodecTest {
 
     @Test
-    public void out(){
+    public void encodeTest(){
         EmbeddedChannel embeddedChannel = new EmbeddedChannel();
         embeddedChannel.pipeline().addLast(
                 new LoggingHandler(),
                 new ChatMessageCustomCodec()
         );
 
+        //encode
         LoginRequestMessage loginRequestMessage = new LoginRequestMessage("ersut", "123", "王");
         embeddedChannel.writeOutbound(loginRequestMessage);
+    }
+
+    @Test
+    public void decodeTest(){
+        EmbeddedChannel embeddedChannel = new EmbeddedChannel();
+        embeddedChannel.pipeline().addLast(
+                new ChatMessageCustomCodec()
+        );
+
+        //要发送的消息
+        ByteBuf sendByteBuf = ByteBufAllocator.DEFAULT.buffer();
+        LoginRequestMessage loginRequestMessage = new LoginRequestMessage("ersut", "123", "王");
+        //将 loginRequestMessage 通过 encode 方法写到 sendByteBuf
+        new ChatMessageCustomCodec().encode(null,loginRequestMessage,sendByteBuf);
+
+        //decode
+        embeddedChannel.writeInbound(sendByteBuf);
+
+    }
+    //发生半包的解决方案
+    @Test
+    public void decodeHalfPackTest(){
+        EmbeddedChannel embeddedChannel = new EmbeddedChannel();
+        embeddedChannel.pipeline().addLast(
+                //todo 添加处理器
+                new ChatMessageCustomCodec()
+        );
+
+        //要发送的消息
+        ByteBuf sendByteBuf = ByteBufAllocator.DEFAULT.buffer();
+        LoginRequestMessage loginRequestMessage = new LoginRequestMessage("ersut", "123", "王");
+        //将 loginRequestMessage 通过 encode 方法写到 sendByteBuf
+        new ChatMessageCustomCodec().encode(null,loginRequestMessage,sendByteBuf);
+
+        //todo 模拟半包
+        embeddedChannel.writeInbound(sendByteBuf);
+
     }
 
 }
