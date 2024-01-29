ec0fbb455f90da77785a882e542248b53c5c592c
wangerfei <83889573@qq.com>
2024-01-29 10:29:36 +0800

    【netty】自定义协议-聊天业务的session部分和service部分，来源于黑马netty视频

---
+++ netty/netty_demo/src/main/java/top/ersut/protocol/chat/server/service/UserServiceFactory.java
@@ -0,0 +1,13 @@
+package top.ersut.protocol.chat.server.service;
+
+public abstract class UserServiceFactory {
+
+    private static UserService userService = new UserServiceMemoryImpl();
+
+    public static UserService getUserService()
+    {
+        return userService;
+    }
+
+
+}
