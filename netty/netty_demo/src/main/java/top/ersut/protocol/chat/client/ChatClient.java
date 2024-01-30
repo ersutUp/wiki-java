6655286c9cf175a824c4d1d2c6c917a0edd97aed
wangerfei <83889573@qq.com>
2024-01-30 09:33:26 +0800

    【netty】自定义协议-聊天业务-finally内添加 shutdownGracefully

--- netty/netty_demo/src/main/java/top/ersut/protocol/chat/client/ChatClient.java
+++ netty/netty_demo/src/main/java/top/ersut/protocol/chat/client/ChatClient.java
@@ -23,72 +23,78 @@ public class ChatClient {
 
     //日志处理器
     static final LoggingHandler LOGGING_HANDLER = new LoggingHandler();
+
     public static void main(String[] args) {
-        Bootstrap bootstrap = new Bootstrap();
-
-        ChannelFuture channelFuture = bootstrap
-                .group(new NioEventLoopGroup())
-                .channel(NioSocketChannel.class)
-                .handler(new ChannelInitializer<NioSocketChannel>() {
-                    @Override
-                    protected void initChannel(NioSocketChannel ch) throws Exception {
-                        ch.pipeline().addLast(
-                                //LTC解码器，解决半包粘包的问题
-                                new LengthFieldBasedFrameDecoder(1024, 12, 4, 0, 0),
-                                LOGGING_HANDLER,
-                                CHAT_MESSAGE_CUSTOM_CODEC_SHARABLE_HANDLER
-                        );
-                        ch.pipeline().addLast(new ChannelInboundHandlerAdapter(){
-                            @Override
-                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                                log.info("msg:{}",msg);
-                                if(msg instanceof LoginResponseMessage){
-                                    LoginResponseMessage loginResponseMessage = (LoginResponseMessage) msg;
-                                    log.info(loginResponseMessage.getReason());
-                                    ctx.channel().close();
-                                }
-                                super.channelRead(ctx, msg);
-                            }
-
-                            @Override
-                            public void channelActive(ChannelHandlerContext ctx) throws Exception {
-                                super.channelActive(ctx);
-
-                                new Thread(()->{
-                                    Scanner scanner = new Scanner(System.in);
-                                    System.out.print("请输入用户名：");
-                                    String username = scanner.nextLine();
-                                    System.out.print("请输入密码：");
-                                    String password = scanner.nextLine();
-
-                                    LoginRequestMessage loginRequestMessage = new LoginRequestMessage(username,password);
-
-                                    ctx.writeAndFlush(loginRequestMessage);
-
-                                },"system in").start();
-                            }
-                        });
-
-                    }
-
-                })
-                .connect(new InetSocketAddress("127.0.0.1", 18808));
-        Channel channel = channelFuture.channel();
-        channelFuture.addListener(future -> {
-            if (future.isSuccess()) {
-                log.info("连接服务端成功");
-            } else {
-                log.error("连接服务端失败");
-                channel.close();
-            }
-        });
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
 
 
         try {
+            Bootstrap bootstrap = new Bootstrap();
+            ChannelFuture channelFuture = bootstrap
+                    .group(eventLoopGroup)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(
+                                    //LTC解码器，解决半包粘包的问题
+                                    new LengthFieldBasedFrameDecoder(1024, 12, 4, 0, 0),
+                                    LOGGING_HANDLER,
+                                    CHAT_MESSAGE_CUSTOM_CODEC_SHARABLE_HANDLER
+                            );
+                            ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                                @Override
+                                public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                    log.info("msg:{}", msg);
+                                    if (msg instanceof LoginResponseMessage) {
+                                        LoginResponseMessage loginResponseMessage = (LoginResponseMessage) msg;
+                                        log.info(loginResponseMessage.getReason());
+                                        ctx.channel().close();
+                                    }
+                                    super.channelRead(ctx, msg);
+                                }
+
+                                @Override
+                                public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                    super.channelActive(ctx);
+
+                                    new Thread(() -> {
+                                        Scanner scanner = new Scanner(System.in);
+                                        System.out.print("请输入用户名：");
+                                        String username = scanner.nextLine();
+                                        System.out.print("请输入密码：");
+                                        String password = scanner.nextLine();
+
+                                        LoginRequestMessage loginRequestMessage = new LoginRequestMessage(username, password);
+
+                                        ctx.writeAndFlush(loginRequestMessage);
+
+                                    }, "system in").start();
+                                }
+                            });
+
+                        }
+
+                    })
+                    .connect(new InetSocketAddress("127.0.0.1", 18808));
+            Channel channel = channelFuture.channel();
+            channelFuture.addListener(future -> {
+                if (future.isSuccess()) {
+                    log.info("连接服务端成功");
+                } else {
+                    log.error("连接服务端失败");
+                    channel.close();
+                }
+            });
+
             channel.closeFuture().sync();
         } catch (InterruptedException e) {
             log.error("关闭客户端失败");
             throw new RuntimeException(e);
+        } finally {
+            if (!eventLoopGroup.isShuttingDown()) {
+                eventLoopGroup.shutdownGracefully();
+            }
         }
 
     }
--- netty/netty_demo/src/main/java/top/ersut/protocol/chat/server/ChatServer.java
+++ netty/netty_demo/src/main/java/top/ersut/protocol/chat/server/ChatServer.java
@@ -33,54 +33,61 @@ public class ChatServer {
         EventLoopGroup works = new NioEventLoopGroup();
 
         ServerBootstrap serverBootstrap = new ServerBootstrap();
-        ChannelFuture bindFuture = serverBootstrap
-                .group(boss, works)
-                .channel(NioServerSocketChannel.class)
-                .childHandler(new ChannelInitializer<NioSocketChannel>() {
-                    @Override
-                    protected void initChannel(NioSocketChannel ch) throws Exception {
-                        ch.pipeline().addLast(
-                                //LTC解码器，解决半包粘包的问题
-                                new LengthFieldBasedFrameDecoder(1024, 12, 4, 0, 0),
-                                LOGGING_HANDLER,
-                                CHAT_MESSAGE_CUSTOM_CODEC_SHARABLE_HANDLER
-
-                        );
-                        ch.pipeline().addLast(
-                                new SimpleChannelInboundHandler<LoginRequestMessage>() {
-
-                                    @Override
-                                    protected void channelRead0(ChannelHandlerContext ctx, LoginRequestMessage msg) throws Exception {
-                                        LoginResponseMessage loginResponseMessage;
-
-                                        UserService userService = UserServiceFactory.getUserService();
-                                        if (userService.login(msg.getAccount(), msg.getPassword())) {
-                                            loginResponseMessage = new LoginResponseMessage(true,"登录成功");
-                                        } else {
-                                            loginResponseMessage = new LoginResponseMessage(false,"登录成功");
-                                        }
-
-                                        ctx.writeAndFlush(loginResponseMessage);
-                                    }
-                                }
-                        );
-                    }
-                }).bind(18808);
-
-        Channel channel = bindFuture.channel();
-        bindFuture.addListener(future -> {
-            if (future.isSuccess()) {
-                log.info("服务端启动成功");
-            } else {
-                log.error("服务端启动失败");
-            }
-        });
-
         try {
+            ChannelFuture bindFuture = serverBootstrap
+                    .group(boss, works)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(
+                                    //LTC解码器，解决半包粘包的问题
+                                    new LengthFieldBasedFrameDecoder(1024, 12, 4, 0, 0),
+                                    LOGGING_HANDLER,
+                                    CHAT_MESSAGE_CUSTOM_CODEC_SHARABLE_HANDLER
+
+                            );
+                            ch.pipeline().addLast(
+                                    new SimpleChannelInboundHandler<LoginRequestMessage>() {
+
+                                        @Override
+                                        protected void channelRead0(ChannelHandlerContext ctx, LoginRequestMessage msg) throws Exception {
+                                            LoginResponseMessage loginResponseMessage;
+
+                                            UserService userService = UserServiceFactory.getUserService();
+                                            if (userService.login(msg.getAccount(), msg.getPassword())) {
+                                                loginResponseMessage = new LoginResponseMessage(true, "登录成功");
+                                            } else {
+                                                loginResponseMessage = new LoginResponseMessage(false, "登录成功");
+                                            }
+
+                                            ctx.writeAndFlush(loginResponseMessage);
+                                        }
+                                    }
+                            );
+                        }
+                    }).bind(18808);
+
+            Channel channel = bindFuture.channel();
+            bindFuture.addListener(future -> {
+                if (future.isSuccess()) {
+                    log.info("服务端启动成功");
+                } else {
+                    log.error("服务端启动失败");
+                }
+            });
+
             channel.closeFuture().sync();
         } catch (InterruptedException e) {
             log.error("关闭服务端失败");
             throw new RuntimeException(e);
+        } finally {
+            if (!boss.isShuttingDown()) {
+                boss.shutdownGracefully();
+            }
+            if (!works.isShuttingDown()) {
+                works.shutdownGracefully();
+            }
         }
 
     }
